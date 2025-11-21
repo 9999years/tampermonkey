2cbe0834c8310dfb3130ba0e3834a168da861e2f
Rebecca Turner <rbt@sent.as>
2025-11-21 11:17:41 -0800

    review-in-graphite: add graphite icon to button

--- review-in-graphite.user.js
+++ review-in-graphite.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Review in Graphite
 // @namespace    http://github.com/9999years/tampermonkey
-// @version      2025-11-21
+// @version      2025-11-21v2
 // @description  Add a button to GitHub PRs which goes to Graphite
 // @author       Rebecca Turner
 // @match        https://github.com/*/pull/*
 // @icon         https://app.graphite.com/favicon.ico
+// @resource     icon https://app.graphite.com/favicon.svg
 // @run-at       document-idle
+// @grant        GM_getResourceURL
 // ==/UserScript==
 
 (function() {
@@ -23,7 +25,14 @@
         const link = document.createElement("a");
         link.className = "flex-md-order-2 Button--secondary Button--small Button m-0 mr-md-0";
         link.href = reviewInGraphiteURL().href;
-        link.innerHTML = `<span class="Button-content"><span class="Button-label">Review in Graphite</span></span>`;
+        link.innerHTML = `
+            <span class="Button-content">
+                <span class="Button-visual Button-leadingVisual">
+                    <img class="octicon octicon-code" width="16">
+                </span>
+            <span class="Button-label">Review in Graphite</span>
+        </span>`;
+        link.querySelector('img').src = GM_getResourceURL("icon");
         return link;
     }
 
